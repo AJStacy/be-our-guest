91be61a89638a6e7c3bb7d05d44770ae98804646
Stacy <ndstc@a483e7d1bd68.ant.amazon.com>
2021-09-20 14:06:25 -0400

    Updated code formatting of test data object-a with Prettier.

--- test/data/object-a.ts
+++ test/data/object-a.ts
@@ -1,13 +1,11 @@
 export class ObjectA {
-
   private myNumber: number;
 
   constructor(num: number) {
     this.myNumber = num;
-  } 
-
-  public returnMyNumber():number {
-    return this.myNumber;
   }
 
+  public returnMyNumber(): number {
+    return this.myNumber;
+  }
 }
